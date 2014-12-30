0ff246eb7c3417514d09360e7a59c728b6abca47
Lorenzo Cioni <lore.cioni@gmail.com>
2014-12-30 10:48:11 +0100

    feature extractor

---
+++ clustering/features/FeatureExtractor.cpp
@@ -0,0 +1,15 @@
+/*
+ * FeatureExtractor.cpp
+ *
+ *  Created on: 30/dic/2014
+ *      Author: lorenzocioni
+ *
+ ***
+ *** LOOP: L
+ ***
+ *
+ */
+
+
+
+
---
+++ clustering/features/FeatureExtractor.h
@@ -0,0 +1,15 @@
+/*
+ * FeatureExtractor.h
+ *
+ *  Created on: 30/dic/2014
+ *      Author: lorenzocioni
+ */
+
+#ifndef CLUSTERING_FEATURES_FEATUREEXTRACTOR_H_
+#define CLUSTERING_FEATURES_FEATUREEXTRACTOR_H_
+
+
+
+
+
+#endif /* CLUSTERING_FEATURES_FEATUREEXTRACTOR_H_ */
