6f151eb4480a03f2e5eda40ef1d3f7327f41db5f
Lorenzo Cioni <lore.cioni@gmail.com>
2014-12-30 10:42:39 +0100

    feature folder

---
+++ clustering/features/LoopFeature.cpp
@@ -0,0 +1,10 @@
+/*
+ * LoopFeature.cpp
+ *
+ *  Created on: 30/dic/2014
+ *      Author: lorenzocioni
+ */
+
+
+
+
---
+++ clustering/features/LoopFeature.h
@@ -0,0 +1,15 @@
+/*
+ * LoopFeature.h
+ *
+ *  Created on: 30/dic/2014
+ *      Author: lorenzocioni
+ */
+
+#ifndef CLUSTERING_FEATURES_LOOPFEATURE_H_
+#define CLUSTERING_FEATURES_LOOPFEATURE_H_
+
+
+
+
+
+#endif /* CLUSTERING_FEATURES_LOOPFEATURE_H_ */
